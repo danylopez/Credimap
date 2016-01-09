1378ae8b7084a2b36a916ab281577741a9a9ccf7
Alan H. Zacarias <alanh.lhp@gmail.com>
2016-01-08 20:59:58 -0600

    Update send.php

--- bootstrap3-contact-form-master/send.php
+++ bootstrap3-contact-form-master/send.php
@@ -1,18 +1,34 @@
 <?php
+$headers .= "From: <cliente@credimap.com>\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8\r\n";
 
 $to = 'alanh.lhp@gmail.com';
-$email = $_POST['email'];
-$subject = 'Interesado en Credito'
+$name = $_POST['name'];
 $phone = $_POST['phone'];
+$email = $_POST['email'];
+$subject = 'Interesado en Credito';
 $message = $_POST['message'];
-$headers = 'F'
+$body = "
+<html>
+<body>
+<p>
+$message
+<h1>Datos Personales</h1>
+Nombre: $name<br/>
+Email: $email<br/>
+Teléfono: $phone<br/>
+</p>
+</body>
+</html>
+";
 
-$body = "This is an automated messgae. \n\n $message";
+//$body = "$message <br><br> Datos Personales <br><br> Nombre: $name<br> Email: $email<br> Teléfono: $phone";
 
-$from = "From: $email";
-
-mail($to, $subject, $body, $from);
-
-echo "Message Sent! <a href='email.html'>Click Here</a> to sent another email"
+if(mail($to, $subject, $body, $headers)){
+	echo "Mensaje enviado! <a href='email.html'>Click Aqui</a> para enviar otro mensaje";
+} else{
+	echo "Hubo un error enviando el mensaje";
+}
 
 ?>
