8c6c3be86eeafc18fb318d066639e50121b1583d
octaviodiaz <octavian.diaz@gmail.com>
2016-01-09 14:33:39 -0600

    Update de la calculadora

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,8 @@
  $(function() {
 
-        var $document = $(document);
-        var selector = '[data-rangeslider]';
-        var $element = $(selector);
-
-      
-
-        // Basic rangeslider initialization
-        $element.rangeslider({
+        var amount = $('#amountSlider');
+        //rangeslider initialization
+        amount.rangeslider({
 
             // Deactivate the feature detection
             polyfill: false,
@@ -15,20 +10,52 @@
             // Callback function
             onInit: function() {
                // valueOutput(this.$element[0]);
-            	console.log(this.$element[0]);
+
             },
 
             // Callback function
             onSlide: function(position, value) {
-                console.log('onSlide');
-                console.log('position: ' + position, 'value: ' + value);
+                $('#amountText').val(value);
             },
-
             // Callback function
             onSlideEnd: function(position, value) {
-                console.log('onSlideEnd');
-                console.log('position: ' + position, 'value: ' + value);
+          
             }
         });
 
     });
+
+$(document).ready(function (){
+    //fixing bug add-on currency size
+    debugger;
+    var h =  $('#pesosSymbolSpan').height();
+    var h2 =  $('#amountText').height();
+    $('#pesosSymbolSpan').height( $('#amountText').height());
+    allowJustNumbers();
+    
+    
+});
+
+
+function allowJustNumbers(){
+
+    $('#amountText').keydown(function (e){
+         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+             // Allow: Ctrl+C
+            (e.keyCode == 67 && e.ctrlKey === true) ||
+             // Allow: Ctrl+X
+            (e.keyCode == 88 && e.ctrlKey === true) ||
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+}
