188fff99997974208125c579c7e36a8d12801b99
Daniel <spmc_egpdalm@hotmail.com>
2016-01-08 21:43:31 -0600

    Datos Mapa
    Datos Mapa

--- js/map.js
+++ js/map.js
@@ -1,7 +1,9 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
+    center: {lat: 19.3202176, lng: -99.224016},
+    scrollwheel: false,
+    zoom: 13,
+    disableDefaultUI: false
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
 
@@ -14,7 +16,7 @@ function initMap() {
       };
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('Aquí estas.');
       map.setCenter(pos);
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
@@ -28,6 +30,71 @@ function initMap() {
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
+                        'ERROR: No encontramos su posicion' :
                         'Error: Your browser doesn\'t support geolocation.');
 }
+/*
+function initMap() {
+  var city = {lat: 19.3202176, lng: -99.224016};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: city,
+    zoom: 13
+  });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      city = pos;
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Aquí estas.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  infowindow = new google.maps.InfoWindow();
+
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: city,
+    radius: 500,
+    types: ['banks']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'ERROR: No encontramos su posicion' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}*/
