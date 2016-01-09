6daba6f4df659e4cc8e411ee5c2f7e3e010e86c1
Alan H. Zacarias <alanh.lhp@gmail.com>
2016-01-08 21:01:27 -0600

    Update send.php

--- bootstrap3-contact-form-master/send.php
+++ bootstrap3-contact-form-master/send.php
@@ -1,29 +1,27 @@
 <?php
-$headers .= "From: <cliente@credimap.com>\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8\r\n";
+	$headers .= "From: <cliente@credimap.com>\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8\r\n";
 
-$to = 'alanh.lhp@gmail.com';
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$subject = 'Interesado en Credito';
-$message = $_POST['message'];
-$body = "
-<html>
-<body>
-<p>
-$message
-<h1>Datos Personales</h1>
-Nombre: $name<br/>
-Email: $email<br/>
-Teléfono: $phone<br/>
-</p>
-</body>
-</html>
-";
-
-//$body = "$message <br><br> Datos Personales <br><br> Nombre: $name<br> Email: $email<br> Teléfono: $phone";
+	$to = 'alanh.lhp@gmail.com';
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$email = $_POST['email'];
+	$subject = 'Interesado en Credito';
+	$message = $_POST['message'];
+	$body = "
+	<html>
+		<body>
+			<p>
+				$message
+				<h1>Datos Personales</h1>
+				Nombre: $name<br/>
+				Email: $email<br/>
+				Teléfono: $phone<br/>
+			</p>
+		</body>
+	</html>
+	";
 
 if(mail($to, $subject, $body, $headers)){
 	echo "Mensaje enviado! <a href='email.html'>Click Aqui</a> para enviar otro mensaje";
