74ec0305076b2c360915d3d8249f858ac3a2687f
octaviodiaz <octavian.diaz@gmail.com>
2016-01-30 22:35:54 -0600

    dany super mega line breaked in 3 sil'tu plait

--- js/financial_entities.js
+++ js/financial_entities.js
@@ -18,15 +18,20 @@ function drawOnTable(){
         newFeRow.append('<td class="col-md-1">' +fe.name + '</td>' );
         newFeRow.append('<td class="col-md-3">' +fe.info.address + '</td>' );
         newFeRow.append('<td class="col-md-1">' +fe.info.phone + '</td>' );
-        newFeRow.append('<td class="col-md-1">' +fe.info.website+ '</td>' );
+        newFeRow.append('<td class="col-md-1" style="max-width: 100px !important; word-wrap: break-word">' +fe.info.website+ '</td>' );
         newFeRow.append('<td class="col-md-1">' +fe.financial.totalPayment + '</td>' );
         newFeRow.append('<td class="col-md-1">' +fe.financial.payment + '</td>' );
         newFeRow.append('<td class="col-md-1">' +fe.financial.taxes + '</td>' );
         newFeRow.append('<td class="col-md-1">' +fe.financial.tax_rate + '</td>' );
-        newFeRow.append('<td class="col-md-2"> <button id="localizeBest" type="button" class="btn btn-default " aria-label="Left Align"   onclick="localizeBest();" data-toggle="tooltip"  data-placement="bottom" title="Localiza en el mapa la entidad financiera según el préstamo consultado"><a id="goFinancial" href="#financial" style="display:hidden"></a>&nbsp;<i class="fa fa-map-marker"></i></button><br> <button id="reqBank" type="button" class="btn btn-default " aria-label="Left Align"   onclick="" data-toggle="tooltip"  data-placement="bottom" title="Muestra los requerimientos del préstamo"><a id="goRrequirements" href="#requerimientos" style="display:hidden"> </a><i class="fa fa-book"></i></button> <br> <button type="button" class="btn btn-default " aria-label="Left Align" onclick="setEntity()" data-toggle="tooltip"  data-placement="bottom" title="Podemos ayudar!"> <i class="fa fa-envelope"></i></button></td> ');
-         newFeRow.append('</tr>');
+        newFeRow.append('<td class="col-md-2">' + '<button id="localizeBest" type="button" class="btn btn-default " aria-label="Left Align"   ' +
+                            'onclick="localizeBest();" data-toggle="tooltip"  data-placement="bottom" title="Localiza en el mapa la entidad financiera según el préstamo consultado">' +
+                            '<a id="goFinancial" href="#financial" style="display:hidden"></a>&nbsp;<i class="fa fa-map-marker"></i></button>' +
+        '                   <button id="reqBank" type="button" class="btn btn-default " aria-label="Left Align"   onclick="" data-toggle="tooltip"  data-placement="bottom" title="Muestra los requerimientos del préstamo"><a id="goRrequirements" href="#requerimientos" style="display:hidden"> </a><i class="fa fa-book"></i></button>  <button type="button" class="btn btn-default " aria-label="Left Align" onclick="setEntity()" data-toggle="tooltip"  data-placement="bottom" title="Podemos ayudar!"> <i class="fa fa-envelope"></i></button></td> ');
+                         newFeRow.append('</tr>');
         $('#feProcessedTable tr:last').after(newFeRow);   
     }
+
+    $('[data-toggle="tooltip"]').tooltip();
 }
 
 function downloadExcel(){
