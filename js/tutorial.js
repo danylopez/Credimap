034e129aba375e6c409a66ee3fe6e7c39dfa5312
Daniel <spmc_egpdalm@hotmail.com>
2016-02-02 11:13:44 -0600

    Pagina Intro

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
+    $("#sitio").click(function(){
+        $("#intro").hide();
+    });
+    $('#sitioaux').click(function() {
+        $("#intro").hide();
+    });
     /* Get iframe src attribute value i.e. YouTube video url
     and store it in a variable */
-    var url = $("#video").attr('src');
+    var url = $("#videoaux").attr('src');
     
     /* Assign empty url value to the iframe src attribute when
     modal hide, which stop the video playing */
     $("#tutorial").on('hide.bs.modal', function(){
-        $("#video").attr('src', '');
+        $("#videoaux").attr('src', '');
     });
     
     /* Assign the initially stored url back to the iframe src
     attribute when modal is displayed again */
     $("#tutorial").on('show.bs.modal', function(){
-        $("#video").attr('src', url);
+        $("#videoaux").attr('src', url);
     });
 });
