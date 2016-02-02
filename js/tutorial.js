16803e7bd86cd477185fd6ff225d9a1392de031f
Daniel <spmc_egpdalm@hotmail.com>
2016-02-02 15:34:13 -0600

    Bug fixed video intro

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
     $("#sitio").click(function(){
-        $("#intro").hide();
         $("#video").attr('src', '');
+        $("#intro").hide();
     });
     $('#sitioaux').click(function() {
-        $("#intro").hide();
         $("#video").attr('src', '');
+        $("#intro").hide();
     });
+    
     /* Get iframe src attribute value i.e. YouTube video url
     and store it in a variable */
     var url = $("#videoaux").attr('src');
@@ -16,10 +17,11 @@ $(document).ready(function(){
     $("#tutorial").on('hide.bs.modal', function(){
         $("#videoaux").attr('src', '');
     });
-    
+
     /* Assign the initially stored url back to the iframe src
     attribute when modal is displayed again */
     $("#tutorial").on('show.bs.modal', function(){
         $("#videoaux").attr('src', url);
     });
+
 });
